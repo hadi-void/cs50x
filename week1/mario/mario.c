0213ac864a8d1cbe425989a7ff0047e94c38a4f7
hadi-void <hadi.m.desk@gmail.com>
2026-06-18 00:22:08 +0500

    Add Week 1 Mario — pyramid logic complete

---
+++ week1/mario/mario.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main (void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while(!(height >= 1 && height <= 8));
+
+
+
+    // blocks
+    for(int i = 0; i < height; i++)
+    {
+
+        for(int k = height - 1; k > i; k--)
+        {
+            printf(" ");
+        }
+
+
+        for(int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
+
+        printf("  ");
+
+
+        for(int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
+
+        printf("\n");
+    }
+}
